a6374a1da143c06e9058f70d9f237c9f14375277
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-27 10:28:18 -0300

    Aula59

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -10,20 +10,28 @@ const MyForms = () => {
   const handleName = (e) => {
     setName(e.target.value)
   }
-  console.log(name)
-  console.log(email)
+  //console.log(name)
+  //console.log(email)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Enviando o formulario')
+    console.log(name, email)
+  }
 
   return (
     <div>
+        {/* 5 - Envio de form */}
         {/* 1 - Criação de Form */}
-        <form >
+        <form onSubmit={handleSubmit} >
           <div>
             <label htmlFor="name">Nome: </label>
             <input type="text" name='name' placeholder='Digite o seu nome:' onChange={handleName} />
           </div>
           {/* 2 - Label envolvendo o Input (sugerida na doc do React) */}
           <label>
-            <span>e-mail: </span>
+            <span>e-mail:</span>
+            {/* 4 - Simplificando a manipulação de state */}
             <input type="email" name='email' placeholder='e-mail:' onChange={(e) => setEmail(e.target.value) } />
           </label>
           <input type="submit" value='Enviar' />
