764e048be3fab9e9b0815432e6e3156dfbe97918
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-27 10:11:32 -0300

    Aula57

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,14 +1,24 @@
 import './MyForm.css';
+import { useState } from 'react';
 
 
 const MyForms = () => {
+  // 3 - Gerenciamento de dados
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  console.log(name)
+
   return (
     <div>
         {/* 1 - Criação de Form */}
         <form >
           <div>
             <label htmlFor="name">Nome: </label>
-            <input type="text" name='name' placeholder='Digite o seu nome: ' />
+            <input type="text" name='name' placeholder='Digite o seu nome:' onChange={handleName} />
           </div>
           {/* 2 - Label envolvendo o Input (sugerida na doc do React) */}
           <label>
