4336122b5df4889b2934fd9ef83146a214ae9ed4
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-27 10:02:35 -0300

    Aula56

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -10,6 +10,11 @@ const MyForms = () => {
             <label htmlFor="name">Nome: </label>
             <input type="text" name='name' placeholder='Digite o seu nome: ' />
           </div>
+          {/* 2 - Label envolvendo o Input (sugerida na doc do React) */}
+          <label>
+            <span>e-mail: </span>
+            <input type="email" name='email' placeholder='e-mail: ' />
+          </label>
           <input type="submit" value='Enviar' />
         </form>
     </div>
